09e4070e3f704b6b3e2ca365d3629a1dc378a31b
matrrrix <lwolff2014@gmail.com>
2023-09-21 18:37:57 -0700

    reverted changes

--- ReplyApp.py
+++ ReplyApp.py
@@ -14,14 +14,14 @@ Users must create an OpenAI account and provide their OpenAI API Key down below,
 
 """
 
-#user_key = st.text_input('Provide your OpenAI API Key', type="password")
+user_key = st.text_input('Provide your OpenAI API Key', type="password")
 
 user_prompt = st.text_input('What is the Canvas Discussion Prompt?')
 
 token_number = st.slider('Select the number of tokens that you wish to generate (think word count). Read above for pricing.', min_value=50, max_value=500, value=150, step=1)
 
-if st.button("Generate", disabled=((user_prompt is None) or (token_number is None))):
-    openai.api_key = api_key
+if st.button("Generate", disabled=((user_key is None) or (user_prompt is None) or (token_number is None))):
+    openai.api_key = user_key
 
     user_prompt = user_prompt + " ->"
 
